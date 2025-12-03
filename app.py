99ec433c9b375c4b897344d8065ff7e2b2a7596c
jasonheldman-creator <jasonheldman@gmail.com>
2025-12-03 15:58:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-app.py
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Waves Simple Console")
+
+st.title("🌊 WAVES SIMPLE CONSOLE")
+
+uploaded_file = st.file_uploader("Upload your CSV")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.subheader("CSV Preview")
+    st.dataframe(df)
+else:
+    st.info("Upload a CSV file to begin.")
