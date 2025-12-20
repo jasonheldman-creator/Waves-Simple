48e6aa63dba3a3106a500d2026c3a265c1d45e1e
jasonheldman-creator <jasonheldman@gmail.com>
2025-12-20 12:50:25 -0500

    Restore `app.py` to its last good state prior to a804a7935fff34cd66a77080ca8ea9b06e951afe.

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-I don't have access to the content directly from the commit hash. You might need to provide it.
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Waves App!"
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.json
+    return data if data else "No Data Received"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
