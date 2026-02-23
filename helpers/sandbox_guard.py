74eacc8808466d729f33c3ac61b2a8388f86d909
jasonheldman-creator <jasonheldman@gmail.com>
2026-02-23 10:24:53 -0500

    Create sandbox_guard.py

---
+++ helpers/sandbox_guard.py
@@ -0,0 +1,10 @@
+"""
+Temporary sandbox guard stub.
+Restores import compatibility for Streamlit deployment.
+"""
+
+SANDBOX_MODE = True
+REPLAY_MODE = False
+
+def guard_action(*args, **kwargs):
+    return True
