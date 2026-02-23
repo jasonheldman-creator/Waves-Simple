8a5600a6be50ca241d1b4da0aeac3bf9b9cdf16b
jasonheldman-creator <jasonheldman@gmail.com>
2026-02-22 23:31:51 -0500

    Refactor Alpaca dependency into lazy import for CI compatibility

--- helpers/wave_performance.py
+++ helpers/wave_performance.py
@@ -1,6 +1,9 @@
-try:
-    from alpaca.data.historical import StockHistoricalDataClient
-except ImportError:
-    StockHistoricalDataClient = None
+def get_alpaca_client():
+    try:
+        from alpaca.data.historical import StockHistoricalDataClient
+        return StockHistoricalDataClient
+    except ImportError:
+        return None
 
-# Your existing code here...
+# Update all usages of StockHistoricalDataClient as follows:
+# Replace direct instantiation with calls to get_alpaca_client() inside function bodies
